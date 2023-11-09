ce0b53036bb194a79359744a4a6ecbd3328c80b3
marcobusacca <marcobusacca01@gmail.com>
2023-11-09 18:23:03 +0100

    Bonus Completato

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -12,5 +12,13 @@ public class CibiPreferiti {
 		System.out.println("Il mio cibo TOP: " + foodArray[0]);
 		
 		System.out.println("Il mio cibo preferito ma non troppo: " + foodArray[foodArray.length - 1]);
+		
+		
+		// BONUS
+		
+		if (foodArray.length % 2 != 0) { // SE LA LUNGHEZZA DELL'ARRAY è DISPARI, STAMPO IL CIBO DI MEZZA CLASSIFICA
+			
+			System.out.println("Cibo di mezza classifica: " + foodArray[foodArray.length / 2]);
+		}
 	}
 }
