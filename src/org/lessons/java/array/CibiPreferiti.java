de9dc500b7791a9c4c8fcbf1920be19efdaa111d
marcobusacca <marcobusacca01@gmail.com>
2023-11-09 17:59:21 +0100

    Esercizio Completato

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -4,5 +4,13 @@ public class CibiPreferiti {
 
 	public static void main(String[] args) {
 		
+		String[] foodArray = {"Pizza margherita", "Bistecca alla fiorentina", "Pasta Carbonara", "Lasagne", "Arancine", "Polenta"};
+		
+		
+		System.out.println("Lunghezza della classifica: " + foodArray.length);
+		
+		System.out.println("Il mio cibo TOP: " + foodArray[0]);
+		
+		System.out.println("Il mio cibo preferito ma non troppo: " + foodArray[foodArray.length - 1]);
 	}
 }
